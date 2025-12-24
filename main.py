96e7f9eeea0076c3c73015aa762f71bcb6bff5f5
bhokalshamli-ui <bhokalshamli@gmail.com>
2025-12-25 00:15:07 +0530

    Refactor main.py to include threading and HTTP server

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 # Copyright (c) 2025 devgagan : https://github.com/devgaganin.  
 # Licensed under the GNU General Public License v3.0.  
 # See LICENSE file in the repository root for full license text.
-
+import threading
+from http.server import HTTPServer, BaseHTTPRequestHandler
 import asyncio
 from shared_client import start_client
 import importlib
@@ -39,3 +40,4 @@ if __name__ == "__main__":
             loop.close()
         except Exception:
             pass
+
